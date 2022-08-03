9ca78278ee1eef85609a889fcee4b8ddef2488ea
Serge Aleynikov <saleyn@gmail.com>
2022-08-03 06:53:36 -0400

    Fix version

--- one/mix.exs
+++ one/mix.exs
@@ -4,7 +4,7 @@ defmodule MixProject.One do
   def project do
     [
       app:             :one,
-      version:         "1.0",
+      version:         "1.0.0",
       elixir:          "~> 1.13",
       start_permanent: Mix.env() == :prod,
       elixirc_paths:   ["src"],
