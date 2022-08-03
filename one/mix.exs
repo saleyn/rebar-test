3b38242c7f5cca2f819b77a8e0bee94bfda62fc5
Serge Aleynikov <saleyn@gmail.com>
2022-08-03 06:48:06 -0400

    Remove rebar files

---
+++ one/mix.exs
@@ -0,0 +1,13 @@
+defmodule MixProject.One do
+  use Mix.Project
+
+  def project do
+    [
+      app:             :one,
+      version:         "1.0",
+      elixir:          "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      elixirc_paths:   ["src"],
+    ]
+  end
+end
--- three/mix.exs
+++ three/mix.exs
@@ -1,26 +1,36 @@
-defmodule FIX.MixProject do
+defmodule MixProject.Three do
   use Mix.Project
 
   def project do
     [
       app:             :three,
-      version:         "1.0.0",
+      version:         "1.0",
       elixir:          "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps:            deps(),
       elixirc_paths:   ["lib"],
+      releases:        releases(),
     ]
   end
 
   def application do
     [
-      extra_applications: [:logger, :one],
+      extra_applications: [:one],
+    ]
+  end
+
+  def releases do
+    [
+      dev: [
+        include_erts: false,
+        include_executables_for: [],
+      ]
     ]
   end
 
   defp deps do
     [
-      {:one, git: "git@github.com:saleyn/rebar-test.git", branch: "three", sparse: "one"},
+      {:one, git: "git@github.com:saleyn/rebar-test.git", branch: "three", sparse: "one", engine: :mix},
     ]
   end
 end
