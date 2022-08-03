954684a5b447480c98fd2223d2847deeaf3e9ffb
Serge Aleynikov <saleyn@gmail.com>
2022-08-03 06:49:51 -0400

    Rearrage files

--- two/mix.exs
+++ two/mix.exs
@@ -3,7 +3,7 @@ defmodule MixProject.Three do
 
   def project do
     [
-      app:             :three,
+      app:             :two,
       version:         "1.0",
       elixir:          "~> 1.13",
       start_permanent: Mix.env() == :prod,
--- two/src/two.erl
+++
@@ -1,5 +0,0 @@
--module(two).
--export([hello/0]).
-
-hello() ->
-  "Hello World".
